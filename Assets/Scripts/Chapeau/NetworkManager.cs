a123482a5be18d0324ee73133eebdb6e14a940d9
deangbingham-star <deangbingham@gmail.com>
2026-09-16 10:46:28 -0700

    working on menu

---
+++ Assets/Scripts/Chapeau/NetworkManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager instance;
+    void Awake()
+    {
+        if(instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+
+    void start()
+    {
+        if(!PhotonNetwork.IsConnected){
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        
+    }
+
+    //create room
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    //joinroom
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void changeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server");
+    }
+
+}
+
